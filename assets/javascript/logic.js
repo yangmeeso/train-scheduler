218b01873c6993b19eeec133598dba64d8fb3ee5
yangmeeso <yangmeeso@gmail.com>
2017-12-10 00:20:26 -0800

    complete logic.js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -53,8 +53,26 @@ database.ref().on("child_added", function (childSnapshot, preventChildKey) {
   var displayTime = displayFirstTrain.split(":");
   var trainTime = moment().hours(displayTime[0]).minutes(displayTime[1]);
   var maxTime = moment.max(moment(), trainTime);
-})
+  var arrival;
+  var minutes;
 
+
+if (maxTime === trainTime) {
+  arrival = trainTime.format("hh:mm A");
+  minutes = trainTime.diff(moment(), "minutes");
+}
+else {
+  var diffTime = moment().diff(trainTime, "minutes");
+  var modulus = diffTime % displayFreq;
+  minutes = displayFreq - modulus;
+  arrival = moment().add(minutes, "m").format("hh:mm A");
+}
+
+$("#employee-table > tbody").append("<tr><td>" + displayName + "</td><td>"
++ displayDest + "</td><td>" + displayFreq + "</td><td>" + arrival + "</td><td>"
++ minutes + "</td></tr>");
+
+});
 
 
 
